420dfd3742db8eec93f8f883eb3647131f723257
Dan <bdylan998@gmail.com>
2019-06-25 17:03:06 +0300

    created sprite node Red, blue, texturedSpriteNode, used position, zPosition

--- SpriteKit Demo/GameScene.swift
+++ SpriteKit Demo/GameScene.swift
@@ -11,9 +11,28 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    let myFirstNode = SKNode()
+    let myFirstSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200, height: 200))
+    let myFirstTexturedSpriteNode = SKSpriteNode(imageNamed: "Spaceship")
+    let myBlueBox = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100, height: 100))
     
     override func didMove(to view: SKView) {
         
-       
+        addChild(myFirstNode)
+        
+        myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        myFirstSpriteNode.anchorPoint = CGPoint(x: 0, y: 0)
+        addChild(myFirstSpriteNode)
+        
+        myFirstTexturedSpriteNode.zPosition = 1
+        myFirstTexturedSpriteNode.size = CGSize(width: 100.0, height: 100.0)
+        myFirstSpriteNode.addChild(myFirstTexturedSpriteNode)
+        
+        myBlueBox.zPosition = 2
+        myBlueBox.position = CGPoint(x: myFirstSpriteNode.size.width/2, y: myFirstSpriteNode.size.height/2)
+        myFirstSpriteNode.addChild(myBlueBox)
+        
+        
+        
     }
 }
